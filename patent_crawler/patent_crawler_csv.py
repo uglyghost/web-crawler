0606593faece10336e9dfd878567b79aa43f2e56
Xingyan Chen <chenxingyan@bupt.edu.cn>
2021-12-04 17:51:28 +0800

    new way to crawler

--- patent_crawler/patent_crawler_csv.py
+++ patent_crawler/patent_crawler_csv.py
@@ -12,27 +12,39 @@ from bson import ObjectId
 filePath = 'H:\\web-crawler1\\patent_data\\'
 
 # 连接服务器
-client = MongoClient('localhost', 27017)
+user = 'wenshu'
+pwd = '123456'
+host = '10.220.139.140'
+port = '27017'
+db_name = 'wenshu'
+
+uri = "mongodb://%s:%s@%s" % (user, pwd, host + ":" + port + "/" + db_name)
+
+# 连接数据库
+
+client = MongoClient(uri)
+# client = MongoClient('localhost', 27017)
 mongodb = client.patent
 
 
 def first_word(text: str) -> str:
     return re.search("([\w']+)", text).group(1)
 
+# 初始化chrome驱动配置
+chrome_options = webdriver.ChromeOptions()
+# 让浏览器不显示自动化测试
+chrome_options.add_argument("-–lang=zh-CN")
+# chrome_options.add_argument("--headless")
+prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': filePath}
+chrome_options.add_experimental_option('prefs', prefs)
+# 设置window系统下的chrome驱动程序
+chromedriver = webdriver.Chrome(executable_path='./driver/chromedriver.exe', options=chrome_options)
+
 
 # web交互式查询函数，
 # input   查询条件keyword
 # return  chrome web driver
 def search(url):
-    # 初始化chrome驱动配置
-    chrome_options = webdriver.ChromeOptions()
-    # 让浏览器不显示自动化测试
-    chrome_options.add_argument("-–lang=zh-CN")
-    chrome_options.add_argument("--headless")
-    prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': filePath}
-    chrome_options.add_experimental_option('prefs', prefs)
-    # 设置window系统下的chrome驱动程序
-    chromedriver = webdriver.Chrome(executable_path='./driver/chromedriver.exe', options=chrome_options)
 
     flag = False  # 是否找到专利 1: 找到  0: 未找到
 
@@ -40,12 +52,27 @@ def search(url):
         chromedriver.get(url)
         time.sleep(3)
 
+        try:
+            number = chromedriver.find_element_by_xpath('/html/body/search-app/search-results/search-ui/div/div/div['
+                                               '2]/div/div/div[1]/div[2]/div[1]/span[1]/span[3]').text
+            print(number)
+        except:
+            print("未找到任何专利")
+            #chromedriver.close()
+            return flag
+
         chromedriver.find_element_by_xpath('//*[@id="count"]/div[1]/span[2]/a').click()
+        dl_wait = True
+        while dl_wait:
+            time.sleep(1)
+            for fname in os.listdir(filePath):
+                if fname.endswith('.csv'):
+                    dl_wait = False
+
         flag = True
-        time.sleep(20)
-        chromedriver.quit()
+        #chromedriver.close()
     except:
-        chromedriver.quit()
+        #chromedriver.close()
         flag = False
 
     return flag
@@ -65,16 +92,14 @@ def renameCSV(comName):
 
     os.rename(oldDirPath, newDirPath)
 
-    # os.remove(oldDirPath)  # 删除原文件
-
     return newDirPath
 
 
 def saveData(newDirPath):
     collection = mongodb['patent_list']
 
-    print('开始数据存储：{}', datetime.datetime.now())
-
+    # print('开始数据存储：{0}'.format(datetime.datetime.now()))
+    patentNum = 0
     with open(newDirPath, 'r', encoding="utf-8") as csvfile:
         next(csvfile)
         reader = csv.reader(csvfile)
@@ -96,8 +121,13 @@ def saveData(newDirPath):
                 else:
                     collection.update_one(oneData, {'$set': query})
                 oneData = {}
+                patentNum += 1
 
-    print('结束数据存储：{}', datetime.datetime.now())
+    os.remove(newDirPath)  # 删除原文件
+
+    # print('结束数据存储：{0}'.format(datetime.datetime.now()))
+
+    return patentNum
 
 
 if __name__ == '__main__':
@@ -115,6 +145,8 @@ if __name__ == '__main__':
     for name_tmp in allcompany:
         comName = name_tmp['company_name']
 
+        # print('开始查找公司：{0}'.format(comName))
+
         url = 'https://patents.google.com/?assignee=' + comName + '&language=CHINESE'
 
         # 查找到制定网页
@@ -122,8 +154,11 @@ if __name__ == '__main__':
         if Flag:
             newDirPath = renameCSV(comName)
 
-            newDirPath = 'H:\\web-crawler1\\patent_data\\华为.csv'
-            saveData(newDirPath)
+            patentNum = saveData(newDirPath)
+        else:
+            patentNum = 0
+
+        print('{0}公司找到{1}个专利'.format(comName, patentNum))
 
         processNum += 1
         tmp.update_one(query, {'$set': {"num": processNum}})
